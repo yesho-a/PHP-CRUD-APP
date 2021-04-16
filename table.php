2215555a11975b6684308bd2eaf4ca0f35582953
simon <sepkap123@gmail.com>
2021-04-16 21:36:10 +0300

    initial commit

---
+++ table.php
@@ -0,0 +1,21 @@
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("localhost", "root", "", "demo");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+// Attempt insert query execution
+$sql = "INSERT INTO employees (name, address,salary ) VALUES ('Peter Parker', 'Solna 24',5000)";
+if(mysqli_query($link, $sql)){
+    echo "Records inserted successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
+?>
